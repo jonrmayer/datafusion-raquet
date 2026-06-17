f9d5e83d5d759fd976bf840ba824f2facffd3c50
jonrmayer <jonrmayer@yahoo.com>
2026-06-17 05:01:07 +0100

    python tests and added functions

--- crates/datafusion-raquet-python/src/udf/quadbin.rs
+++ crates/datafusion-raquet-python/src/udf/quadbin.rs
@@ -1,49 +1,58 @@
 use pyo3::prelude::*;
 
 use datafusion_raquet::udf::quadbin::{
-    QuadBinFromLonLat, QuadBinFromTile, QuadBinKRing, QuadBinResolution, QuadBinToChildren,
-    QuadBinToParent, QuadBinToSibling,
+    QuadBinFromLonLat, QuadBinFromTile, QuadBinKRing, QuadBinResolution, QuadBinToBBOX,
+    QuadBinToBBOXMercator, QuadBinToBBOXWGS84, QuadBinToChildren, QuadBinToGeoJSON,
+    QuadBinToLonLat, QuadBinToParent, QuadBinToSibling, QuadBinToTile, QuadBinToWKT,
 };
 
 use crate::impl_udf;
 
 impl_udf!(QuadBinFromTile, PyQuadBinFromTile, "QuadBinFromTile");
+
+impl_udf!(QuadBinToTile, PyQuadBinToTile, "QuadBinToTile");
 impl_udf!(QuadBinFromLonLat, PyQuadBinFromLonLat, "QuadBinFromLonLat");
 impl_udf!(QuadBinToParent, PyQuadBinToParent, "QuadBinToParent");
 impl_udf!(QuadBinResolution, PyQuadBinResolution, "QuadBinResolution");
 impl_udf!(QuadBinToChildren, PyQuadBinToChildren, "QuadBinToChildren");
 impl_udf!(QuadBinToSibling, PyQuadBinToSibling, "QuadBinToSibling");
 impl_udf!(QuadBinKRing, PyQuadBinKRing, "QuadBinKRing");
-// impl_udf!(
-//     QuadBinToBBOXMercator,
-//     PyQuadBinToBBOXMercator,
-//     "QuadBinToBBOXMercator"
-// );
-// impl_udf!(
-//     QuadBinToBBOXWGS84,
-//     PyQuadBinToBBOXWGS84,
-//     "QuadBinToBBOXWGS84"
-// );
-// impl_udf!(QuadBinToLonLat, PyQuadBinToLonLat, "QuadBinToLonLat");
+
+impl_udf!(QuadBinToBBOX, PyQuadBinToBBOX, "QuadBinToBBOX");
+
+impl_udf!(
+    QuadBinToBBOXMercator,
+    PyQuadBinToBBOXMercator,
+    "QuadBinToBBOXMercator"
+);
+impl_udf!(
+    QuadBinToBBOXWGS84,
+    PyQuadBinToBBOXWGS84,
+    "QuadBinToBBOXWGS84"
+);
+impl_udf!(QuadBinToLonLat, PyQuadBinToLonLat, "QuadBinToLonLat");
 // impl_udf!(QuadBinToPixelXY, PyQuadBinToPixelXY, "QuadBinToPixelXY");
-// impl_udf!(QuadBinToWKT, PyQuadBinToWKT, "QuadBinToWKT");
-// impl_udf!(QuadBinToGeoJSON, PyQuadBinToGeoJSON, "QuadBinToGeoJSON");
+impl_udf!(QuadBinToWKT, PyQuadBinToWKT, "QuadBinToWKT");
+impl_udf!(QuadBinToGeoJSON, PyQuadBinToGeoJSON, "QuadBinToGeoJSON");
 
 #[pymodule]
 pub(crate) fn quadbin(m: &Bound<PyModule>) -> PyResult<()> {
     m.add_class::<PyQuadBinFromTile>()?;
+    m.add_class::<PyQuadBinToTile>()?;
     m.add_class::<PyQuadBinFromLonLat>()?;
     m.add_class::<PyQuadBinToParent>()?;
     m.add_class::<PyQuadBinResolution>()?;
     m.add_class::<PyQuadBinToChildren>()?;
     m.add_class::<PyQuadBinToSibling>()?;
     m.add_class::<PyQuadBinKRing>()?;
-    // m.add_class::<PyQuadBinToBBOXMercator>()?;
-    // m.add_class::<PyQuadBinToBBOXWGS84>()?;
-    // m.add_class::<PyQuadBinToLonLat>()?;
+
+    m.add_class::<PyQuadBinToBBOX>()?;
+    m.add_class::<PyQuadBinToBBOXMercator>()?;
+    m.add_class::<PyQuadBinToBBOXWGS84>()?;
+    m.add_class::<PyQuadBinToLonLat>()?;
     // m.add_class::<PyQuadBinToPixelXY>()?;
-    // m.add_class::<PyQuadBinToWKT>()?;
-    // m.add_class::<PyQuadBinToGeoJSON>()?;
+    m.add_class::<PyQuadBinToWKT>()?;
+    m.add_class::<PyQuadBinToGeoJSON>()?;
 
     Ok(())
 }
---
+++ crates/datafusion-raquet/src/udf/quadbin/quadbin_to_bbox.rs
@@ -0,0 +1,177 @@
+use std::any::Any;
+use std::sync::{Arc, OnceLock};
+
+use arrow_array::builder::{
+    ArrayBuilder, Float64Builder, ListBuilder, StructBuilder, UInt64Builder,
+};
+use arrow_array::cast::AsArray;
+use arrow_array::types::{Int64Type, UInt8Type, UInt32Type, UInt64Type};
+use arrow_array::{Array, ArrayRef, GenericListArray, ListArray, StructArray, UInt64Array};
+use arrow_schema::{DataType, Field, FieldRef, Fields};
+
+
+
+use datafusion::error::{DataFusionError, Result};
+use datafusion::logical_expr::scalar_doc_sections::DOC_SECTION_OTHER;
+use datafusion::logical_expr::{
+    ColumnarValue, Documentation, ReturnFieldArgs, ScalarFunctionArgs, ScalarUDFImpl, Signature,
+    TypeSignature, Volatility,
+};
+
+use crate::error::{RaquetDataFusionError, RaquetDataFusionResult};
+
+
+use quadbin_rs::{Tile, cell_to_tile, tile_to_bbox_wgs84};
+
+#[derive(Debug, Eq, PartialEq, Hash)]
+pub struct QuadBinToBBOX {
+    signature: Signature,
+}
+
+impl QuadBinToBBOX {
+    pub fn new() -> Self {
+        Self {
+            signature: Signature::one_of(
+                vec![
+                    TypeSignature::Exact(vec![DataType::Int64]),
+                    TypeSignature::Exact(vec![DataType::Int64, DataType::Int64]),
+                ],
+                Volatility::Immutable,
+            ),
+        }
+    }
+}
+
+impl Default for QuadBinToBBOX {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+static DOCUMENTATION: OnceLock<Documentation> = OnceLock::new();
+
+impl ScalarUDFImpl for QuadBinToBBOX {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn name(&self) -> &str {
+        "quadbin_to_bbox"
+    }
+
+    fn signature(&self) -> &Signature {
+        &self.signature
+    }
+
+    fn return_type(&self, _arg_types: &[DataType]) -> Result<DataType> {
+        Err(DataFusionError::Internal("return_type".to_string()))
+    }
+
+    fn return_field_from_args(&self, args: ReturnFieldArgs) -> Result<FieldRef> {
+        Ok(return_field_impl(args)?)
+    }
+    fn invoke_with_args(&self, args: ScalarFunctionArgs) -> Result<ColumnarValue> {
+        let arrays = ColumnarValue::values_to_arrays(&args.args)?;
+        let cell_arr = build_cell_array(arrays)?;
+        let array_ref: ArrayRef = Arc::new(cell_arr);
+        Ok(ColumnarValue::Array(array_ref))
+    }
+
+    fn documentation(&self) -> Option<&Documentation> {
+        Some(DOCUMENTATION.get_or_init(|| {
+            Documentation::builder(
+                DOC_SECTION_OTHER,
+                "Return a Bbox Struct from a quadbin cell ",
+                "quadbin_to_bbox(5256690695657226239) ",
+            )
+            .with_argument("cell", "cell value")
+            .build()
+        }))
+    }
+}
+
+fn return_field_impl(_args: ReturnFieldArgs) -> RaquetDataFusionResult<FieldRef> {
+    let min_lon = Field::new("min_lon", DataType::Float64, false);
+    let min_lat = Field::new("min_lat", DataType::Float64, false);
+    let max_lon = Field::new("max_lon", DataType::Float64, false);
+    let max_lat = Field::new("max_lat", DataType::Float64, false);
+
+    let fields = Fields::from(vec![min_lon, min_lat, max_lon, max_lat]);
+    let bbox = Field::new_struct("", fields, false);
+   
+    Ok(Arc::new(bbox))
+}
+
+fn build_cell_array(arrays: Vec<ArrayRef>) -> RaquetDataFusionResult<StructArray> {
+    let cells = arrays[0].as_primitive::<Int64Type>();
+    let mut xmin_builder = Float64Builder::new();
+    let mut ymin_builder = Float64Builder::new();
+    let mut xmax_builder = Float64Builder::new();
+    let mut ymax_builder = Float64Builder::new();
+
+    for cell in cells.iter() {
+        let tile: Tile = cell_to_tile(cell.unwrap() as u64);
+        let bbox_wgs84 = tile_to_bbox_wgs84(tile);
+        xmin_builder.append_value(bbox_wgs84.min_x);
+        ymin_builder.append_value(bbox_wgs84.min_y);
+        xmax_builder.append_value(bbox_wgs84.max_x);
+        ymax_builder.append_value(bbox_wgs84.max_y);
+    }
+
+    let values_fields = vec![
+        Field::new("min_lon", DataType::Float64, false),
+        Field::new("min_lat", DataType::Float64, false),
+        Field::new("max_lon", DataType::Float64, false),
+        Field::new("max_lat", DataType::Float64, false),
+    ];
+
+    let fields = Fields::from(values_fields);
+
+    let arrays: Vec<ArrayRef> = vec![
+        Arc::new(xmin_builder.finish()),
+        Arc::new(ymin_builder.finish()),
+        Arc::new(xmax_builder.finish()),
+        Arc::new(ymax_builder.finish()),
+    ];
+    let nulls = None;
+    let arr = StructArray::new(fields, arrays, nulls);
+    // let mut vcells: Vec<Abbox> = vec![];
+    // for cell in cells.iter() {
+    //     let tile: Tile = cell_to_tile(cell.unwrap() as u64);
+    //     let bbox_wgs84 = tile_to_bbox_wgs84(tile);
+    //     let abox: Abbox = Abbox::new(bbox_wgs84);
+    //     vcells.push(abox);
+    // }
+    // let box_array: ArrayRef = vcells.try_into_arrow().unwrap();
+    // let struct_array = box_array
+    //     .as_any()
+    //     .downcast_ref::<arrow::array::StructArray>()
+    //     .unwrap();
+    // Ok(struct_array.clone())
+     Ok(arr)
+}
+
+#[cfg(test)]
+mod tests {
+    use datafusion::prelude::SessionContext;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_quadbin_to_children() {
+        let ctx = SessionContext::new();
+        ctx.register_udf(QuadBinToBBOX::default().into());
+        let sql = r#"SELECT quadbin_to_bbox_wgs84(5256690695657226239) ;"#;
+        println!("{:?}", sql);
+
+        let df = ctx.sql(sql).await.unwrap();
+       df.show().await.unwrap();
+        // let column = batches[0].column(0);
+        // // let string_arr = column.as_string_view();
+
+        // let val = column.as_list(0).value(0);
+        // println!("{:?}", val);
+    }
+
+   
+}
