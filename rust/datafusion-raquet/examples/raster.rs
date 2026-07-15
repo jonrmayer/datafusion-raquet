54d83a8dc4635a26609ce9a7468a8f7c67ec1de6
jonrmayer <jonrmayer@yahoo.com>
2026-07-15 13:09:07 +0100

    df 53.1

--- rust/datafusion-raquet/examples/raster.rs
+++ rust/datafusion-raquet/examples/raster.rs
@@ -1,5 +1,6 @@
 use crate::udf::general::register as general_register;
 use crate::udf::raster::register as raster_register;
+use crate::udf::quadbin::register as quadbin_register;
 use datafusion::prelude::*;
 use datafusion_raquet::*;
 use std::sync::Arc;
@@ -19,6 +20,7 @@ pub async fn get_ctx() -> SessionContext {
     let ctx = SessionContext::new_with_config(SessionConfig::new().with_information_schema(true));
     raster_register(&ctx);
     general_register(&ctx);
+    quadbin_register(&ctx);
     let sql = r###"
         CREATE EXTERNAL TABLE solar
         STORED AS PARQUET
@@ -32,11 +34,11 @@ pub async fn test_native_tile_1000(ctx: SessionContext) -> Vec<arrow_array::Reco
     let sql = r###"
 
   with data as (
-    SELECT cast_raquet(band_1,'256', 'Separated', 'float32','NaN','gzip') band from solar where block<>0 
+    SELECT binary_to_raquet(band_1,'256', 'Separated', 'float32','NaN','gzip') band from solar where block<>0 
     limit 1000
     )
 
-    select native_tile(band) native from data"###;
+    select raquet_band_native(band) native from data"###;
 
     let df = ctx.sql(sql).await.unwrap();
     df.collect().await.unwrap()
@@ -46,7 +48,7 @@ pub async fn test_statistics_tile_1000(ctx: SessionContext) -> Vec<arrow_array::
     let sql = r###"
 
    with data as (
-   SELECT cast_raquet(band_1,'256', 'Separated', 'float32','NaN','gzip') band from solar where block<>0 
+   SELECT binary_to_raquet(band_1,'256', 'Separated', 'float32','NaN','gzip') band from solar where block<>0 
     limit 1000
    
     )
